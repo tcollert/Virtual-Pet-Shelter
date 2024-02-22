024cf11b38191e1b97cdfae8e65d8dbf350f4551
tcollert <tcollert1@gmail.com>
2024-02-21 20:38:55 -0500

    done with virtual pet

--- src/main/java/shelter/VirtualPetShelterApp.java
+++ src/main/java/shelter/VirtualPetShelterApp.java
@@ -9,12 +9,27 @@ public class VirtualPetShelterApp {
 
         Scanner input = new Scanner(System.in);
 
+        VirtualPetShelter sonicsPlace = new VirtualPetShelter();
         VirtualPet sonic = new VirtualPet();
+
+        sonic.setSonicsPal1("Tails");
+        sonic.setSonicsPal2("Sally");
+        sonic.setSonicsPal3("Knuckles");
         sonic.setPetName("Sonic The Hedgehog");
-        sonic.setPetBoredomLevel(50);
-        sonic.setPetHungerLevel(50);
-        sonic.setPetThirstLevel(50);
+        sonic.setSonicsBoredomLevel(50);
+        sonic.setSonicsHungerLevel(50);
+        sonic.setSonicsThirstLevel(50);
+
+        sonicsPlace.sonicAndFriends.add("Sonic");
+        sonicsPlace.sonicAndFriends.add("Tails");
+        sonicsPlace.sonicAndFriends.add("Sally");
+        sonicsPlace.sonicAndFriends.add("Knuckles");
+
         String petName = sonic.getPetName();
+        String sonicsPal1 = sonic.getSonicsPal1();
+        String sonicsPal2 = sonic.getSonicsPal2();
+        String sonicsPal3 = sonic.getSonicsPal3();
+
         System.out.println("Hey there! My name is " + petName + " What is your name?");
         System.out.println("                     _.-*'\"      \"`*-._                   \r\n" + //
                 "                _.-*'                  `*-._              \r\n" + //
@@ -45,54 +60,61 @@ public class VirtualPetShelterApp {
                 "               `*=--..--=*'");
 
         String newFriendsName = input.nextLine();
-        System.out.println("It is great to meet you " + newFriendsName + "!" + " I can't wait to hang out!");
-        sonic.getPetDescription();
+        System.out.println(
+                "It is great to meet you " + newFriendsName + "!" + " My friends and I cannot wait to hang out!");
+        sonic.setSonicsDescription("Fun fact, my friends call me the blue blur. I am super fast and super blue!");
+        System.out.println(sonic.getSonicsDescription());
+        System.out.println(newFriendsName + ", I would like you to meet my friends. They are: " + sonicsPal1
+                + sonicsPal2 + sonicsPal3);
 
         while (true) {
 
             System.out.println("What should we do today?");
-            System.out.println(">> [0] Hang out");
-            System.out.println(">> [1] Scarf down some chili dogs.");
-            System.out.println(">> [2] Chill and have some Vitamin water.");
-            System.out.println(">> [3] Quit.");
-            System.out.println("My boredom level is: " + sonic.getPetBoredomLevel());
-            System.out.println("My thirst level is: " + sonic.getPetThirstLevel());
-            System.out.println("My hunger level is: " + sonic.getPetHungerLevel());
+            System.out.println(">> [0] Hang out with one of us");
+            System.out.println(">> [1] Scarf down some chili dogs with the group.");
+            System.out.println(">> [2] Chill and have some Vitamin water with us.");
+            System.out.println(">> [3] Adopt a friend to spend the day with.");
+            System.out.println(">> [4] Bring a new friend for us at Sonic's Place.");
+            System.out.println(">> [5] Quit.");
+            System.out.println("Check out our levels to help you decide what you want to do first.");
+            System.out.println("Sonic's boredom level is: " + sonic.getSonicsBoredomLevel());
+            System.out.println("Sonic's thirst level is: " + sonic.getSonicsThirstLevel());
+            System.out.println("Sonic's hunger level is: " + sonic.getSonicsHungerLevel());
             int choice = input.nextInt();
 
             if (choice == 3) {
                 System.out.println("Sorry to see you go!");
                 break;
             }
-            int minLevel = sonic.getPetBoredomLevel();
+            int minLevel = sonic.getSonicsBoredomLevel();
             if (minLevel < 0) {
-                sonic.setPetBoredomLevel(minLevel + 15);
+                sonic.setSonicsBoredomLevel(minLevel + 15);
             }
-            int maxLevel = sonic.getPetBoredomLevel();
+            int maxLevel = sonic.getSonicsBoredomLevel();
             if (maxLevel > 100) {
-                sonic.setPetBoredomLevel(maxLevel - 15);
+                sonic.setSonicsBoredomLevel(maxLevel - 15);
             }
             if (choice == 0) {
                 sonic.running();
             }
-            int minLevel2 = sonic.getPetHungerLevel();
+            int minLevel2 = sonic.getSonicsHungerLevel();
             if (minLevel2 < 0) {
-                sonic.setPetHungerLevel(minLevel2 + 15);
+                sonic.setSonicsHungerLevel(minLevel2 + 15);
             }
-            int maxLevel2 = sonic.getPetHungerLevel();
+            int maxLevel2 = sonic.getSonicsHungerLevel();
             if (maxLevel2 > 100) {
-                sonic.setPetHungerLevel(maxLevel2 - 15);
+                sonic.setSonicsHungerLevel(maxLevel2 - 15);
             }
             if (choice == 1) {
                 sonic.feedMe();
             }
-            int minLevel3 = sonic.getPetThirstLevel();
+            int minLevel3 = sonic.getSonicsThirstLevel();
             if (minLevel3 < 0) {
-                sonic.setPetThirstLevel(minLevel3 + 15);
+                sonic.setSonicsThirstLevel(minLevel3 + 15);
             }
-            int maxLevel3 = sonic.getPetThirstLevel();
+            int maxLevel3 = sonic.getSonicsThirstLevel();
             if (maxLevel3 > 100) {
-                sonic.setPetThirstLevel(maxLevel3 - 15);
+                sonic.setSonicsThirstLevel(maxLevel3 - 15);
 
             } else if (choice == 2) {
                 sonic.hydration();
