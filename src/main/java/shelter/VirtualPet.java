95665582124ec7162abfbbdd87e2af24c8626f30
tcollert <tcollert1@gmail.com>
2024-02-21 21:06:21 -0500

    virtual pey

--- src/main/java/shelter/VirtualPet.java
+++ src/main/java/shelter/VirtualPet.java
@@ -2,47 +2,59 @@ package shelter;
 
 public class VirtualPet {
 
-    private String sonicsDescription;
     private String petName;
-    private int sonicsHungerLevel;
-    private int tailsHungerLevel;
-    private int sallysHungerLevel;
-    private int knucklesHungerLevel;
-    private int sonicsThirstLevel;
-    private int tailsThirstLevel;
-    private int sallysThirstLevel;
-    private int knucklesThirstLevel;
-    private int sonicsBoredomLevel;
-    private int tailsBoredomLevel;
-    private int sallysBoredomLevel;
-    private int knucklesBoredomLevel;
-    private String sonicsPal1;
-    private String sonicsPal2;
-    private String sonicsPal3;
+    private String petDescription;
+    private int petHungerLevel;
+    private int petThirstLevel;
+    private int petBoredomLevel;
 
-    public String setDescription(String petDescription) {
+    public VirtualPet(String petName, String petDescription) {
+        this.petName = petName;
+        this.petDescription = petDescription;
+        this.petHungerLevel = 50;
+        this.petThirstLevel = 50;
+        this.petBoredomLevel = 50;
+    }
 
-        this.sonicsDescription = petDescription;
-
-        return sonicsDescription;
+    public VirtualPet(String petName, String petDescription, int petHungerLevel, int petThirstLevel,
+            int petBoredomLevel) {
+        this.petName = petName;
+        this.petDescription = petDescription;
+        this.petHungerLevel = petHungerLevel;
+        this.petThirstLevel = petThirstLevel;
+        this.petBoredomLevel = petBoredomLevel;
     }
 
     public void feedMe() {
-        int feedMe = this.getSonicsHungerLevel();
+        int feedMe = this.getPetHungerLevel();
         int updatedHungerLevel = feedMe - 10;
-        this.setSonicsHungerLevel(updatedHungerLevel);
+        this.setPetHungerLevel(updatedHungerLevel);
     }
 
     public void hydration() {
-        int hydration = this.getSonicsThirstLevel();
+        int hydration = this.getPetThirstLevel();
         int updatedThirstLevel = hydration - 10;
-        this.setSonicsThirstLevel(updatedThirstLevel);
+        this.setPetThirstLevel(updatedThirstLevel);
     }
 
     public void running() {
-        int running = this.getSonicsBoredomLevel();
+        int running = this.getPetBoredomLevel();
         int updatedBoredomLevel = running - 10;
-        this.setSonicsBoredomLevel(updatedBoredomLevel);
+        this.setPetBoredomLevel(updatedBoredomLevel);
+    }
+
+    public void tick() {
+        // increase this calling object's thirst, hunger, boredom -using getters and
+        // setters
+
+        int currentHungerLevel = this.getPetHungerLevel();
+        this.setPetHungerLevel(currentHungerLevel + 1);
+
+        int currentThirstLevel = this.getPetThirstLevel();
+        this.setPetThirstLevel(currentThirstLevel + 1);
+
+        int currentBoredomLevel = this.getPetBoredomLevel();
+        this.setPetBoredomLevel(currentBoredomLevel + 1);
     }
 
     public String getPetName() {
@@ -53,145 +65,36 @@ public class VirtualPet {
         this.petName = petName;
     }
 
-    public void tick() {
-        // increase this calling object's thirst, hunger, boredom -using getters and
-        // setters
-
-        int currentHungerLevel = this.getSonicsHungerLevel();
-        this.setSonicsHungerLevel(currentHungerLevel + 1);
-
-        int currentThirstLevel = this.getSonicsThirstLevel();
-        this.setSonicsThirstLevel(currentThirstLevel + 1);
-
-        int currentBoredomLevel = this.getSonicsBoredomLevel();
-        this.setSonicsBoredomLevel(currentBoredomLevel + 1);
+    public int getPetHungerLevel() {
+        return this.petHungerLevel;
     }
 
-    public String getSonicsPal1() {
-        return this.sonicsPal1;
+    public void setPetHungerLevel(int petHungerLevel) {
+        this.petHungerLevel = petHungerLevel;
     }
 
-    public void setSonicsPal1(String sonicsPal1) {
-        this.sonicsPal1 = sonicsPal1;
+    public int getPetThirstLevel() {
+        return this.petThirstLevel;
     }
 
-    public String getSonicsPal2() {
-        return this.sonicsPal2;
+    public void setPetThirstLevel(int petThirstLevel) {
+        this.petThirstLevel = petThirstLevel;
     }
 
-    public void setSonicsPal2(String sonicsPal2) {
-        this.sonicsPal2 = sonicsPal2;
+    public int getPetBoredomLevel() {
+        return this.petBoredomLevel;
     }
 
-    public String getSonicsPal3() {
-        return this.sonicsPal3;
+    public void setPetBoredomLevel(int petBoredomLevel) {
+        this.petBoredomLevel = petBoredomLevel;
     }
 
-    public void setSonicsPal3(String sonicsPal3) {
-        this.sonicsPal3 = sonicsPal3;
+    public String getPetDescription() {
+        return this.petDescription;
     }
 
-    public String getSonicsDescription() {
-        return sonicsDescription;
+    public void setPetDescription(String petDescription) {
+        this.petDescription = petDescription;
     }
 
-    public void setSonicsDescription(String sonicsDescription) {
-        this.sonicsDescription = sonicsDescription;
-    }
-
-    public int getSonicsHungerLevel() {
-        return sonicsHungerLevel;
-    }
-
-    public void setSonicsHungerLevel(int sonicsHungerLevel) {
-        this.sonicsHungerLevel = sonicsHungerLevel;
-    }
-
-    public int getTailsHungerLevel() {
-        return tailsHungerLevel;
-    }
-
-    public void setTailsHungerLevel(int tailsHungerLevel) {
-        this.tailsHungerLevel = tailsHungerLevel;
-    }
-
-    public int getSallysHungerLevel() {
-        return sallysHungerLevel;
-    }
-
-    public void setSallysHungerLevel(int sallysHungerLevel) {
-        this.sallysHungerLevel = sallysHungerLevel;
-    }
-
-    public int getKnucklesHungerLevel() {
-        return knucklesHungerLevel;
-    }
-
-    public void setKnucklesHungerLevel(int knucklesHungerLevel) {
-        this.knucklesHungerLevel = knucklesHungerLevel;
-    }
-
-    public int getSonicsThirstLevel() {
-        return sonicsThirstLevel;
-    }
-
-    public void setSonicsThirstLevel(int sonicThirstLevel) {
-        this.sonicsThirstLevel = sonicThirstLevel;
-    }
-
-    public int getTailsThirstLevel() {
-        return tailsThirstLevel;
-    }
-
-    public void setTailsThirstLevel(int tailsThirstLevel) {
-        this.tailsThirstLevel = tailsThirstLevel;
-    }
-
-    public int getSallysThirstLevel() {
-        return sallysThirstLevel;
-    }
-
-    public void setSallysThirstLevel(int sallysThirstLevel) {
-        this.sallysThirstLevel = sallysThirstLevel;
-    }
-
-    public int getKnucklesThirstLevel() {
-        return knucklesThirstLevel;
-    }
-
-    public void setKnucklesThirstLevel(int knucklesThirstLevel) {
-        this.knucklesThirstLevel = knucklesThirstLevel;
-    }
-
-    public int getSonicsBoredomLevel() {
-        return sonicsBoredomLevel;
-    }
-
-    public void setSonicsBoredomLevel(int sonicsBoredomLevel) {
-        this.sonicsBoredomLevel = sonicsBoredomLevel;
-    }
-
-    public int getTailsBoredomLevel() {
-        return tailsBoredomLevel;
-    }
-
-    public void setTailsBoredomLevel(int tailsBoredomLevel) {
-        this.tailsBoredomLevel = tailsBoredomLevel;
-    }
-
-    public int getSallysBoredomLevel() {
-        return sallysBoredomLevel;
-    }
-
-    public void setSallysBoredomLevel(int sallysBoredomLevel) {
-        this.sallysBoredomLevel = sallysBoredomLevel;
-    }
-
-    public int getKnucklesBoredomLevel() {
-        return knucklesBoredomLevel;
-    }
-
-    public void setKnucklesBoredomLevel(int knucklesBoredomLevel) {
-        this.knucklesBoredomLevel = knucklesBoredomLevel;
-    }
 }
